6d003a6b49dc007fe5172e407c82dc6d7223daf4
Simon Tegg <simon@loomio.org>
2014-09-28 22:24:22 +1300

    kNN for minorities at risk classification

--- fsi.js
+++ fsi.js
@@ -3,8 +3,19 @@ var nor = require('./data/norway');
 var _ = require('lodash');
 var csv = require('csv-parser');
 var fs = require('fs');
+var writeStream = fs.createWriteStream('./data/output.json')
+var matrix = require('array-matrix');
+
 
 var net = new brain.NeuralNetwork();
+
+var mallard = require('mallard');
+var dnn = require('dnn');
+
+var dataset = [];
+var outputset = [];
+
+var output = matrix(20, 20);
 
 fs.createReadStream('./data/fsi-2006.csv')
   .pipe(csv())
@@ -26,18 +37,45 @@ fs.createReadStream('./data/fsi-2006.csv')
       };
     });
 
-    console.log('row', data)
-    net.train([
-      {
-        input: {
-          "Group-greivance": data["Group-greivance"],
-          "Uneven-development": data["Uneven-development"]
-        },
-        output: {"EcDis–4": data["EcDis–4"]}
-      }
-    ]);
+    var category = data["State-discrimination"] === 1 ? "State-discrimination" : "no";
+
+    // var row = [data["Group-greivance"], data["Uneven-development"]];
+    // var output = [data['PolDis–4']];
+    var row = {
+      category: category, 
+      datapoints: [
+        data["Group-greivance"],
+        data["Uneven-development"]
+      ]
+    };
+
+    dataset.push(row);
+    // outputset.push(output);
+    console.log(row)
+
   })
   .on('end', function () {
-    var output = net.run({"Group-greivance": 1, "Uneven-development": 1})
+    // var output = net.run({"Group-greivance": 0, "Uneven-development": 0});
+    // var trainedDataset = mallard.bayes.train(dataset);
+
+
+    for (var i = 0; i < output.length; i++) {
+      for (var j = 0; j < output.length; j++) {
+        var gg = i*0.05;
+        var ud = j*0.05;
+
+        output[i][j] = mallard.kNN.classify([gg, ud], dataset, 7) +': gg-'+gg+' ud-'+ud;
+      }
+    };
+
+
+    
+
     console.log('output', output)
+    console.log(mallard.kNN.classify([0.8,0.9], dataset, 7))
+
+
   });
+
+
+  /// Threshold group grievance: 8.0, Uneven-development: 8.8
