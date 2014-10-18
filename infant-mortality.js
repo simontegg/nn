a8a0bf6202b6b412248ffc968dd45b338685e910
Simon Tegg <simon@loomio.org>
2014-10-18 17:30:17 +1300

    pagination streams

--- infant-mortality.js
+++ infant-mortality.js
@@ -2,14 +2,54 @@ var request = require('request');
 var Transform = require('stream').Transform;
 
 var parser = new Transform({objectMode: true});
+var es = require('event-stream');
+var filter = require('stream-filter');
+var fs = require('fs');
+var  JSONStream = require('JSONStream');
+var _ = require('lodash');
+var paginationStream = require('pagination-stream');
+var restCollection = require('rest-collection-stream')
+var isArray = require('isarray');
 
-parser._transform = function(data, encoding, done) {
-  console.log(data.toString());
-  done();
-};
 
-var url = "http://api.worldbank.org/countries/all/indicators/SP.DYN.IMRT.IN?format=json&page=110"
+var jsonToStrings = JSONStream.stringify(false);
+
+var ws = fs.createWriteStream('test.json', {flags: 'a'})
+
+var countries = require('./node_modules/countries-list/countries.minimal.json')
+var codes = Object.keys(countries)
 
 
-request(url)
-  .pipe(parser)
+var options = {
+  urlFormat: "http://api.worldbank.org/countries/all/indicators/SP.DYN.IMRT.IN?date=1955:2014&format=json&page=%d",
+  headers: {'user-agent': 'pug'},
+  start: 0,
+  end: 279,
+  retries: 2
+}
+
+
+paginationStream(options)
+  .pipe(JSONStream.parse("*.*"))
+  .pipe(filter(function(data) {
+    if (data.country) {
+      return _.contains(codes, data.country.id)
+    } else {
+      return false;
+    }
+  }))
+  .pipe(es.map(function(data, cb) {
+    var newData = {
+      country: data.country.value,
+      code: data.country.id,
+      value: parseFloat(data.value),
+      year: parseInt(data.date)
+    };
+    cb(null, newData)
+  }))
+  .pipe(jsonToStrings)
+  .pipe(ws)
+
+
+
+
